11382ba509499e719ca5f39a17056d4f2d191c7f
You Song Hou <yousonghou@gmail.com>
2021-07-16 21:32:29 -0400

    install fleek-storage-js for uploading HTML file to fleek storage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,65 @@
 <template>
-  <h1>Create Your Own Quiz</h1>
+  <v-container>
+    <h1>Create Your Own Quiz</h1>
+
+    <v-text-field
+      v-model="question1"
+      label="Question"
+      outlined
+      clearable
+    ></v-text-field>
+
+    <v-btn
+      color="primary"
+      elevation="2"
+      x-large
+      @click="uploadFile()"
+    >Create</v-btn>
+
+    <p>{{ publicUrl }}</p>
+  </v-container>
 </template>
 
 <script>
+  import fleekStorage from '@fleekhq/fleek-storage-js'
+
+  import { fleekAPIKey, fleekAPISecret } from '../config';
 
   export default {
     name: 'Home',
+    data: () => ({
+      publicUrl: "",
+      question1: "",
+    }),
+    methods: {
+      async uploadFile(){
+        const HTMLContent = `
+          <!DOCTYPE html>
+          <html lang="en" dir="ltr">
+            <head>
+              <meta charset="utf-8">
+              <title>Skynet Certificate</title>
+              <meta name="description" content="Certificate">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+                <h1>Quiz</h1>
 
-    components: {
-    },
+                <h2>${this.question1}<h2>
+            </body>
+          </html>
+        `
+
+        const uploadedFile = await fleekStorage.upload({
+          apiKey: fleekAPIKey,
+          apiSecret: fleekAPISecret,
+          key: 'test',
+          data: HTMLContent
+        });
+
+        console.log(uploadedFile);
+        this.publicUrl = uploadedFile.publicUrl;
+      }
+    }
   }
 </script>
