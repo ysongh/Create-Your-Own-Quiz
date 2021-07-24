692e55db10ee0871df67f4ad9dbaf18d4f665ee2
You Song Hou <yousonghou@gmail.com>
2021-07-24 00:39:46 -0400

    add a navbar

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar
+    app
+    dense
+    color="primary"
+    dark
+  >
+    <v-icon class="mr-2">mdi-file</v-icon>
+    <v-toolbar-title>
+      Create Your Own Quiz
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="/"
+      text
+    >
+      <span class="mr-2">Home</span>
+      
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style>
+
+</style>
