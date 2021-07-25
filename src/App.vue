c9b01680ea9590b5f778b8509a6c979c06204d69
You Song Hou <yousonghou@gmail.com>
2021-07-25 00:17:32 -0400

    add a footer

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <v-main>
       <router-view/>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import Navbar from './components/layout/Navbar.vue';
+import Footer from './components/layout/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Footer
   },
   data: () => ({
     //
