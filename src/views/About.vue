ce747391b5355e4aaefdce1107ca91a85970cdf6
You Song Hou <yousonghou@gmail.com>
2021-08-19 00:56:53 -0400

    install skynet-js and use it to store file

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,27 +6,63 @@
 </template>
 
 <script>
+import { SkynetClient } from 'skynet-js';
+import { quizTemplate } from '../helpers/quizTemplate';
+
+const portal = 'https://siasky.net/';
+const skynetClient = new SkynetClient(portal);
 
 export default {
   name: "About",
   methods: {
-    redirectToNamebase(){
-    const records_json = [
-      { type: "TXT",
-        host: '_dnslink.time3', 
-        value: "dnslink=ipfs/SITE_CID://bafybeiaugst5lhuwdw2zuv2hlxls5tibnhggp3shp5vigemppp5qybh6ki",
-        ttl: 60 },
-      { type: "CNAME", host: 'time3', value: "gateway.ipfs.io.", ttl: 3600 }]
-                      
-    const tld = 'aaronrasor';
-    const url = new URL(`https://namebase.io/next/domain-manager/${tld}/records`);
-    const redirectUrl = 'http://localhost:8080/about'
-    const encodedRedirectUrl = encodeURIComponent(encodeURIComponent(redirectUrl.toString()));
+    async redirectToNamebase(){
+      const HTMLContent = quizTemplate("quiz123", "Something", "It is a quiz", [{
+        "question": 'What color is the water',
+        "image": '',
+        "size": 'long',
+        "answers": [
+          { text: 'yellow', correct: false },
+          { text: 'blue', correct: true },
+          { text: 'green', correct: false }
+        ]
+      }]);
 
-    url.searchParams.append('records', btoa(JSON.stringify(records_json)));
-    url.searchParams.append('redirect', encodedRedirectUrl);
+      const webDirectory = {
+        'index.html': new File(
+          [HTMLContent],
+          'index.html',
+          {
+            type: 'text/html',
+          }
+        )
+      };
 
-    window.location.href = url.toString();
+      // Upload quiz
+      const data = await skynetClient.uploadDirectory(
+        webDirectory,
+        'quiz'
+      );
+      console.log(data)
+
+      const skylink = `sia://${data.skylink.slice(4)}`;
+      console.log(skylink)
+
+      const records_json = [
+        { type: "TXT",
+          host: '_contenthash.tjijfw', 
+          value: skylink,
+          ttl: 60 },
+        { type: "CNAME", host: 'tjijfw', value: "sia.namebase.io.", ttl: 3600 }]
+                        
+      const tld = 'aaronrasor';
+      const url = new URL(`https://namebase.io/next/domain-manager/${tld}/records`);
+      const redirectUrl = 'http://localhost:8080/about'
+      const encodedRedirectUrl = encodeURIComponent(encodeURIComponent(redirectUrl.toString()));
+
+      url.searchParams.append('records', btoa(JSON.stringify(records_json)));
+      url.searchParams.append('redirect', encodedRedirectUrl);
+
+      window.location.href = url.toString();
     }
   }
 }
